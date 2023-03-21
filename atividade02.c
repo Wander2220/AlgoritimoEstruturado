1bab69d29a469205bb7a7536586c1bc621fe924d
Wander2220 <127902709+Wander2220@users.noreply.github.com>
2023-03-21 20:07:09 -0300

    atividade2

---
+++ atividade02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Caderno de poesias e um belo lugar\n Tantas coisas lindas que eu gostaria de falar\n Eu falo em forma de versos para todos poderem escutar\n Agora voce ja sabe por que os poetas passam os dias escrevendo em seus\n cadernos de poesias");
+    return 0;
+}
