fa11cc0bb26078492106dff131b7ce3e71b6a0ac
Wander2220 <127902709+Wander2220@users.noreply.github.com>
2023-03-21 20:46:50 -0300

    atividade5

---
+++ atividade5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int A, B;
+int result;
+
+    printf("digite o valor de A\n");
+    scanf("%d", &A);
+    printf("digite o valor de B\n");
+    scanf("%d", &B);
+    
+    printf("a soma de A e B eh :%d", A + B);
+
+    return 0;
+}
