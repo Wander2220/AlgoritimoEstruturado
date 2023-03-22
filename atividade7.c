df0d454809eab91fa8cef79e012594aab6d69528
Wander2220 <127902709+Wander2220@users.noreply.github.com>
2023-03-21 21:06:06 -0300

    atividade7

---
+++ atividade7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int arroz, feijao;
+    int PROD;
+
+        printf("digite o valor de A\n");
+        scanf("%d", &arroz);
+        printf("digite o valor de B\n");
+        scanf("%d", &feijao);
+        
+        PROD = arroz * feijao;
+
+        printf("PROD= %d", PROD);
+
+}
